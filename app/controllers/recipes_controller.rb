e6874765430cff75cc6a128e9b65a99c812a6b3b
LIBERCOSOFT <akinspreciousola@gmail.com>
2023-02-08 08:32:48 +0100

    Implement the list of recipes for a logged in user

---
+++ app/controllers/recipes_controller.rb
@@ -0,0 +1,11 @@
+class RecipesController < ApplicationController
+  def index
+    @recipes = Recipe.where(user_id: current_user)
+  end
+
+  def destroy
+    recipe = Recipe.find(params[:id])
+    recipe.destroy
+    redirect_to recipes_path
+  end
+end
