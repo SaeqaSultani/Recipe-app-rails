4f4c7b498c03398af359eadcb718098bdbf4856f
LIBERCOSOFT <akinspreciousola@gmail.com>
2023-02-09 21:06:33 +0100

    Implement add and display of food list

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   resources :recipes, only: [:index, :show, :destroy]
+  resources :foods, only: [:index, :new, :create, :destroy]
   # Defines the root path route ("/")
   # root "articles#index"
 end
