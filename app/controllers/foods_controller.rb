1f9687bb73279bcfe1c610bac1bee7737577dcfc
Saeqa Sultani <saeqasultani@gmail.com>
2023-02-09 21:01:50 +0430

    added the food list

---
+++ app/controllers/foods_controller.rb
@@ -0,0 +1,33 @@
+class FoodsController < ApplicationController
+  def index
+    @foods = Food.all
+  end
+
+  def create
+    @food = Food.new(food_params)
+    @food.user_id = current_user.id
+    
+    if @food.save
+    redirect_to foods_path, notice: "Food was successfully created."
+    else
+      render :new, alert: "Food was not created."
+    end
+  end
+
+  def new
+    @food = Food.new
+  end
+
+  def destroy
+    @food = Food.find(params[:id])    
+    @food.destroy
+    redirect_to foods_path, notice: "Food was successfully deleted."  
+  end
+
+  private
+
+  def food_params
+    params.require(:food).permit(:name, :measurement_unit, :price, :quantity)
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,20 @@
 Rails.application.routes.draw do
+  # get 'foods/index'
+  # get 'foods/create'
+  # get 'foods/new'
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+  # resources :foods, :only => [:index, :create, :new]
   # Defines the root path route ("/")
   # root "articles#index"
+
+  root "foods#index"
+
+  resources :foods, only: [:index, :new, :create, :destroy]
+  resources :recipes, only: [:index, :show, :public, :new, :create, :destroy] do
+    resources :recipe_foods , only: [:new, :index, :create, :destroy]
+  end
+  resources :public_recipes, only: [:index]
+  resources :general_shopping_list, only: [:show]
+
 end
