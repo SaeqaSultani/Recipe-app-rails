96f3ae05f8c1e6a4e9adedcac998a4c7b803d8d2
LIBERCOSOFT <akinspreciousola@gmail.com>
2023-02-10 11:11:16 +0100

    Add recipe foods to recipe details

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -5,6 +5,7 @@ class RecipesController < ApplicationController
 
   def show
     @recipe = Recipe.find(params[:id])
+    @recipe_foods = RecipeFood.where(recipe_id: params[:id])
   end
 
   def destroy
