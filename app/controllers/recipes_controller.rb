67bea438c6d0ed963d65bc0566d399abf936c5c9
LIBERCOSOFT <akinspreciousola@gmail.com>
2023-02-10 13:49:48 +0100

    Add feature to create a new recipe

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,4 +1,17 @@
 class RecipesController < ApplicationController
+  def new
+    @recipe = Recipe.new
+  end
+
+  def create
+    new_recipe = Recipe.new(name: params[:recipe][:name], preparation_time: params[:recipe][:preparation_time], cooking_time: params[:recipe][:cooking_time], description: params[:recipe][:description], public: params[:recipe][:public], user: current_user)
+    if new_recipe.save
+      redirect_to recipes_path, notice: 'Recipe was successfully created.'
+    else
+      render :new, alert: 'Recipe was not created.'
+    end
+  end
+
   def index
     @recipes = Recipe.where(user_id: current_user)
   end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   root to: 'public_recipes#index'
   devise_for :users, controllers: { registrations: "registrations" }
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  resources :recipes, only: [:index, :show, :destroy] do
+  resources :recipes, only: [:new, :create, :index, :show, :destroy] do
     resources :recipe_foods, only: [:new, :create, :destroy]
   end
   resources :foods, only: [:index, :new, :create, :destroy]
