09d6fdb790f1cb30bebf9c8831bafacca6e82979
LIBERCOSOFT <akinspreciousola@gmail.com>
2023-02-10 12:13:07 +0100

    Add delete feature for recipe_food in recipe details page

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   devise_for :users, controllers: { registrations: "registrations" }
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   resources :recipes, only: [:index, :show, :destroy] do
-    resources :recipe_foods, only: [:new, :create]
+    resources :recipe_foods, only: [:new, :create, :destroy]
   end
   resources :foods, only: [:index, :new, :create, :destroy]
   # Defines the root path route ("/")
